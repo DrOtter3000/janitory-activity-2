a8fd5155e9153c498e4e693dbffe2168e7454aa7
drotter3000 <xtristrix@cyberiade.de>
2023-09-07 15:30:09 +0200

    add cambob

--- player.gd
+++ player.gd
@@ -6,21 +6,24 @@ var speed = 5
 var jump_speed = 5
 var mouse_sensitivity = 0.002
 
+#Vars for CamBob
+var direction = Vector3.ZERO
+var _delta = 0.0
+var cb_speed = 7
+var cb_height = 0.5
+@onready var camera = $Camera3D
+@onready var cameraPos :Vector3 = camera.position
+
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _physics_process(delta):
-	velocity.y += -gravity * delta
-	var input = Input.get_vector("left", "right", "forward", "back")
-	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
-	velocity.x = movement_dir.x * speed
-	velocity.z = movement_dir.z * speed
+	movement(delta)
 
-	
-	move_and_slide()
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		velocity.y = jump_speed
+
+func _process(delta):
+	cambob(delta)
 
 
 func _input(event):
@@ -29,3 +32,29 @@ func _input(event):
 		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
 		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(60), deg_to_rad(65))
 
+
+func movement(delta):
+	velocity.y += -gravity * delta
+	var input = Input.get_vector("left", "right", "forward", "back")
+	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
+	velocity.x = movement_dir.x * speed
+	velocity.z = movement_dir.z * speed
+
+	move_and_slide()
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_speed
+	
+	direction = movement_dir
+
+
+func cambob(delta):
+	_delta += delta
+	
+	var cam_bob
+	if direction != Vector3.ZERO:
+		cam_bob = floor (abs(direction.z) + abs(direction.x)) * _delta * cb_speed
+	else:
+		cam_bob = _delta * cb_speed * 0.25
+	
+	var objCam = cameraPos + Vector3.UP * sin(cam_bob) * cb_height
+	camera.position = camera.position.lerp(objCam, delta)
