7836a11dbdc4c6ebf0afe3f2b4b5c3e524436bfe
drotter3000 <xtristrix@cyberiade.de>
2023-09-27 20:06:52 +0200

    change NavigationMesh3D to Static Colliders

--- enemy.gd
+++ enemy.gd
@@ -1,45 +1,66 @@
 extends CharacterBody3D
 
-var speed = 1
+var speed = 3
+
+var status = "patrol"
 var haunting = true
+var patrol_point = Vector3.ZERO
+var list_of_patrol_points = []
 
 @onready var nav: NavigationAgent3D = $NavigationAgent3D
-
 
 var in_line_of_sight : bool
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
+func _ready():
+	randomize()
+	list_of_patrol_points = get_node("../PatrolPositions").get_children()
+	print(list_of_patrol_points)
+	set_new_target_point()
+	print(patrol_point)
+
+
 func _process(delta):
 	
-	
-	if haunting:
-		
-		if not in_line_of_sight:
+	match status:
+		"haunting":
 			var player_position = get_tree().get_first_node_in_group("Player").position
 			var direction = Vector3()
 	
-			$Body.look_at(player_position)
+			$Dingo.look_at(player_position)
 			nav.target_position = player_position
 	
 			direction = nav.get_next_path_position() - global_position
 			direction.y = 0
 			direction = direction.normalized()
-		
+
 			velocity = direction * (speed)
 			velocity.y -= gravity * delta
-		
-			move_and_slide()
 
 
-func _on_visible_on_screen_notifier_3d_screen_entered():
-	in_line_of_sight = true
+		"patrol":
+			var direction = Vector3()
+			
+			$Dingo.look_at(nav.get_next_path_position())
+			
+			nav.target_position = patrol_point.global_position
+			
+			direction = nav.get_next_path_position() - global_position
+			direction.y = 0
+			direction = direction.normalized()
+			
+			velocity = direction * (speed)
+			velocity.y -= gravity * delta
+			
+			if nav.is_navigation_finished():
+				set_new_target_point()
+			
+
+	move_and_slide()
 
 
-func _on_visible_on_screen_notifier_3d_screen_exited():
-	in_line_of_sight = false
+func set_new_target_point():
+	patrol_point = list_of_patrol_points[randi_range(0, list_of_patrol_points.size()-1)]
 
 
-func _on_kill_area_body_entered(body):
-	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://game_over_screen.tscn")
