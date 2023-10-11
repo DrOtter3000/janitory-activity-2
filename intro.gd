58a28155b591a79277219352e1ce874d70bddd92
drotter3000 <xtristrix@cyberiade.de>
2023-10-11 18:49:39 +0200

    add Intro scene

---
+++ intro.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var mouse_sensitivity = 0.03
+
+@onready var driver = $Car/Driver
+@onready var camera = $Car/Driver/Camera3D
+
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		driver.rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+		camera.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
